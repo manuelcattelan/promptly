112520e7bc9e063349ca7c522b63c3ee3d088c77
Manuel Cattelan <cattelanmanuel07@gmail.com>
2023-11-30 13:48:10 +0100

    chore: format code and add explanatory comments

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,42 +1,57 @@
 autoload -U colors && colors
 
+# Prompt's prefix and suffix format whenever git information is available.
 GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
 GIT_PROMPT_SUFFIX="%{$reset_color%} "
 
+# Git status' icons.
 GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{✗%}"
 GIT_PROMPT_CLEAN="%{$fg[blue]%})"
 
+#
+# Promptly's git commands are read-only and should not interfere with other
+# processes. As such, we wrap the git command in a local function instead of
+# exporting the variable directly in order to avoid interfering with
+# manually-run git commands by the user.
+#
 function __promptly_git() {
-  GIT_OPTIONAL_LOCKS=0 command git "$@"
+    GIT_OPTIONAL_LOCKS=0 command git "$@"
 }
 
+#
+# Check whether the user is currently inside a git repository.
+#
 function promptly_git_directory() {
-  if ! __promptly_git rev-parse --is-inside-work-tree &> /dev/null \
-     || [[ "$(__promptly_git config 2>/dev/null)" == 1 ]]; then
-    return 1
-  fi
-
-  return 0
+    if ! __promptly_git rev-parse --is-inside-work-tree &> /dev/null \
+       || [[ "$(__promptly_git config 2>/dev/null)" == 1 ]]; then
+        return 1
+    fi;
+    return 0
 }
 
+#
+# Return the current repository's branch name, if available.
+#
 function promptly_git_branch() {
-  local ref
-  ref=$(__promptly_git symbolic-ref --short HEAD 2> /dev/null) \
-  || ref=$(__promptly_git describe --tags --exact-match HEAD 2> /dev/null) \
-  || ref=$(__promptly_git rev-parse --short HEAD 2> /dev/null) \
-  || return 0
-
-  echo "${ref:gs/%/%%}"
+    local ref
+    ref=$(__promptly_git symbolic-ref --short HEAD 2> /dev/null) \
+        || ref=$(__promptly_git describe --tags --exact-match HEAD 2> /dev/null) \
+        || ref=$(__promptly_git rev-parse --short HEAD 2> /dev/null) \
+        || return 0
+    echo "${ref:gs/%/%%}"
 }
 
-function promptly_git_dirty() {
-  local git_status
-  if [[ "$(__promptly_git config 2> /dev/null)" != "1" ]]; then
-    git_status=$(__promptly_git status --porcelain 2> /dev/null | tail -n 1)
-  fi
-  if [[ -n $git_status ]]; then
-    echo "$GIT_PROMPT_DIRTY"
-  else
-    echo "$GIT_PROMPT_CLEAN"
-  fi
+#
+# Return the icon corresponding to the current repository's status (dirty/clean).
+#
+function promptly_git_status() {
+    local git_status
+    if [[ "$(__promptly_git config 2> /dev/null)" != "1" ]]; then
+        git_status=$(__promptly_git status --porcelain 2> /dev/null | tail -n 1)
+    fi;
+    if [[ -n $git_status ]]; then
+        echo "$GIT_PROMPT_DIRTY"
+    else
+        echo "$GIT_PROMPT_CLEAN"
+    fi;
 }
--- promptly.zsh
+++ promptly.zsh
@@ -6,72 +6,105 @@ source "$PROMPTLY_ROOT/lib/git.zsh"
 autoload -U colors && colors
 autoload -Uz add-zsh-hook
 
+# Initialize async library
 async_init
 
+# Default prompt format
 promptly_default_prompt="%(?:%{$fg_bold[green]%}%1{❯%} :%{$fg_bold[red]%}%1{❯%} ) %{$fg[cyan]%}%c%{$reset_color%}"
 
+#
+# Update prompt format with most recent data and reset it to show changes.
+#
 function promptly_update() {
-  PROMPT="$promptly_default_prompt $promptly_git_data[promptly_git_prefix]$promptly_git_data[promptly_git_branch]$promptly_git_data[promptly_git_dirty]$promptly_git_data[promptly_git_suffix]"
+    local git_prefix=$promptly_git_data[promptly_git_prefix]
+    local git_suffix=$promptly_git_data[promptly_git_suffix]
 
-  zle -R && zle reset-prompt
+    local git_branch=$promptly_git_data[promptly_git_branch]
+    local git_status=$promptly_git_data[promptly_git_status]
+
+    PROMPT="$promptly_default_prompt"
+    PROMPT+=" $git_prefix$git_branch$git_status$git_suffix"
+
+    zle -R && zle reset-prompt
 }
 
+#
+# Initialize async worker and register callback function.
+#
 function promptly_init_worker() {
-  async_start_worker promptly_worker -n
-  async_register_callback promptly_worker promptly_callback
+    async_start_worker promptly_worker -n
+    async_register_callback promptly_worker promptly_callback
 }
 
+#
+# Initialize jobs responsible for retrieving git information.
+#
 function promptly_init_jobs() {
-  typeset -Ag promptly_git_data
+    typeset -Ag promptly_git_data
 
-  local promptly_pwd="$PWD"
-  async_worker_eval promptly_worker builtin cd -q $promptly_pwd
+    local promptly_pwd="$PWD"
+    async_worker_eval promptly_worker builtin cd -q $promptly_pwd
 
-  if (promptly_git_directory); then
-    async_flush_jobs promptly_worker
+    # If user is inside a git repository:
+    #   - flush current jobs
+    #   - setup prompt's prefix and suffix for git information
+    #   - start jobs for retrieving git information
+    if (promptly_git_directory); then
+        async_flush_jobs promptly_worker
 
-    promptly_git_data[promptly_git_prefix]=$GIT_PROMPT_PREFIX
-    promptly_git_data[promptly_git_suffix]=$GIT_PROMPT_SUFFIX
-    
-    async_job promptly_worker promptly_git_branch
-    async_job promptly_worker promptly_git_dirty
-  else
-    promptly_git_data[promptly_git_prefix]= 
-    promptly_git_data[promptly_git_suffix]= 
+        promptly_git_data[promptly_git_prefix]=$GIT_PROMPT_PREFIX
+        promptly_git_data[promptly_git_suffix]=$GIT_PROMPT_SUFFIX
 
-    promptly_git_data[promptly_git_branch]= 
-    promptly_git_data[promptly_git_dirty]= 
-  fi;
+        async_job promptly_worker promptly_git_branch
+        async_job promptly_worker promptly_git_status
+    # Else, reset prompt's git data to remove it from prompt.
+    else
+        promptly_git_data[promptly_git_prefix]= 
+        promptly_git_data[promptly_git_suffix]= 
 
-  promptly_update
+        promptly_git_data[promptly_git_branch]= 
+        promptly_git_data[promptly_git_status]= 
+    fi;
+
+    promptly_update
 }
 
+#
+# Callback function to be ran whenever a job finishes it's execution.
+#
 function promptly_callback() {
-  local job_name=$1
-  local job_return_code=$2
-  local job_output=$3
+    local job_name=$1
+    local job_return_code=$2
+    local job_output=$3
 
-  if (( job_return_code == 2 )) \
-  || (( job_return_code == 3 )) \
-  || (( job_return_code == 130 )); then
-    async_stop_worker promptly_worker
+    # If job did not run successfully, stop current worker and configure
+    # async library from scratch.
+    if (( job_return_code == 2 )) || (( job_return_code == 3 )) || (( job_return_code == 130 )); then
+        async_stop_worker promptly_worker
+
+        promptly_init_worker
+        promptly_init_jobs
+    elif (( job_return_code )); then
+        promptly_init_jobs
+    fi;
+
+    # Store job's output and update prompt with new data.
+    promptly_git_data[$job_name]=$job_output
+    promptly_update
+}
+
+#
+# Setup prompt by configuring async library and prompt format.
+#
+function promptly_setup() {
     promptly_init_worker
     promptly_init_jobs
-  elif (( job_return_code )); then
-    promptly_init_jobs
-  fi;
 
-  promptly_git_data[$job_name]=$job_output
-  promptly_update
-}
+    # Add promptly_init_jobs to functions to be executed before printing prompt.
+    add-zsh-hook precmd promptly_init_jobs
 
-function promptly_setup() {
-  promptly_init_worker
-  promptly_init_jobs
-
-  add-zsh-hook precmd promptly_init_jobs
-
-  PROMPT="$promptly_default_prompt"
+    # Initialize prompt with default configuration while async jobs are executed.
+    PROMPT="$promptly_default_prompt"
 }
 
 promptly_setup
